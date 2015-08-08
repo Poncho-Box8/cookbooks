a167b0d4b1a16cd22c8cd48a0d5c7c8e52b32244
root <root@rails-app2.localdomain>
2015-08-08 20:52:36 +0530

    Removing env specification

--- generate_docs/recipes/default.rb
+++ generate_docs/recipes/default.rb
@@ -1,8 +1,10 @@
 include_recipe 'deploy'
+node[:deploy].each do |application, deploy|
 bash "generate_docs" do
   user "root"
   code <<-EOH
     cd "#{deploy[:deploy_to]}/current/config"
     bundle exec rake swagger:docs RAILS_ENV=production
   EOH
+end
 end

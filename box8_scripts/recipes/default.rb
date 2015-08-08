8808532e9bae60b968a0b6e10d8c0158e20a9565
root <root@rails-app2.localdomain>
2015-08-08 21:58:30 +0530

    Removing env specification

--- box8_scripts/recipes/default.rb
+++ box8_scripts/recipes/default.rb
@@ -1,10 +1,10 @@
 include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
-cookbook_file "../files/script.rh" do
+cookbook_file "../files/script.sh" do
   source "script.sh"
   mode 0755
 end
-execute "script.rb" do
+execute "script.sh" do
   cwd "#{deploy[:deploy_to]}/current/config"  
   command "./script.sh"
 end

aee969354e45b0279de20fa7e87c72ad3c6eaf4d
root <root@ip-10-0-1-55.localdomain>
2015-10-14 14:06:52 +0000

    Adding two new cookbooks

---
+++ callcenter/CHANGELOG.md
@@ -0,0 +1,13 @@
+callcenter CHANGELOG
+====================
+
+This file is used to list changes made in each version of the callcenter cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of callcenter
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ callcenter/recipes/default.rb
@@ -0,0 +1,41 @@
+include recipe apt 
+package 'nginx' do
+  action :install
+end
+service 'nginx' do
+  action [ :enable, :start ]
+end
+cookbook_file "/etc/nginx/sites-available/default" do
+  source "default"
+  mode "0644"
+end
+package 'git' do 
+  action :install
+end
+package 'nodejs' do
+  action :install
+end
+link '/usr/bin/node' do
+  to '/usr/bin/nodejs'
+end
+package 'npm' do
+  action :install
+end
+execute 'update_npm' do
+  command 'npm install npm -g'
+end
+execute 'install_yo' do
+  command 'npm install -g yo'
+end
+execute 'install_bower' do
+  command 'npm install -g bower'
+end
+execute 'install_grunt' do
+  command 'npm install -g grunt'
+end
+execute 'install_grunt_cli' do
+  command 'npm install -g grunt-cli'
+end
+~   
+~   
+

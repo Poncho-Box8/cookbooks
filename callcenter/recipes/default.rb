dcb7e239a5e0a0888b68b495cd622bb76c68e1c8
root <root@callcenterinstance.localdomain>
2015-10-15 06:15:36 +0000

    fix

--- callcenter/recipes/default.rb
+++ callcenter/recipes/default.rb
@@ -1,3 +1,12 @@
 package 'nginx' do
   action :install
 end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source "index.html"
+  mode "0644"
+end

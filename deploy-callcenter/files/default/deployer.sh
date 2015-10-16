fb5c344c95a31952c761fdb42ff46b77afc7cd52
aamirahmad170 <aamir@Aamirs-MacBook-Pro.local>
2015-10-16 20:06:48 +0530

    Push

--- deploy-callcenter/files/default/deployer.sh
+++ deploy-callcenter/files/default/deployer.sh
@@ -1,12 +1,13 @@
-rm -rfv /opt/tmp_store
-mkdir /opt/tmp_store
-cp -r /srv/www/static_app/current/* /opt/tmp_store
-cd /opt/tmp_store
-bower install —allow-root
+cd /srv/www/statc_app/current/
+bower install --allow-root --config.interactive=false
 npm install
-grunt
-rm -rfv /srv/www/static_app/current/*
-cp -r /opt/tmp_store/dist/* /srv/www/static_app/current/
-cp -r /srv/www/statc_app/current/app/fonts/* /srv/www/statc_app/current/dist/fonts/
-cp -r /srv/www/statc_app/current/bower_components/font-awesome/fonts/* /srv/www/statc_app/current/dist/fonts/
-
+grunt 
+rm -rfv /opt/tmp_store/
+mkdir /opt/tmp_store/
+cp -r /srv/www/statc_app/current/* /opt/tmp_store/
+rm -rfv /srv/www/statc_app/current/*
+rm -rfv /srv/www/statc_app/current/.*
+cp -r /opt/tmp_store/dist/* /srv/www/statc_app/current/
+cp -r /opt/tmp_store/app/fonts/* /srv/www/statc_app/current/fonts/
+cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/statc_app/current/fonts/
+chown deploy:www-data /srv/www/statc_app/current/*

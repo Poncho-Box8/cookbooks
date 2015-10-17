f6169e5c0a090b55f3f526a3509acac4402624ec
aamirahmad170 <aamir@Aamirs-MacBook-Pro.local>
2015-10-17 12:44:23 +0530

    Push

--- install-callcenter/files/default/installer.sh
+++ install-callcenter/files/default/installer.sh
@@ -2,7 +2,7 @@ apt-get update -y
 apt-get install nodejs
 ln -s /usr/bin/nodejs /usr/bin/node
 apt-get install npm -y
-apt-get install git nginx -y
+apt-get install git -y
 npm install npm -g  cd /usr/local/lib/node_modules
 curl registry.npmjs.com/npm/-/npm-2.12.0.tgz |tar xz --transform="s:^package:npm:"
 npm i npm -g

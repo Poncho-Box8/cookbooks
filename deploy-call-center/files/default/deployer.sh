9867244a120a19e09521ec95d9e2e9c87eb2116b
aamirahmad170 <aamir@Aamirs-MacBook-Pro.local>
2015-10-17 14:33:54 +0530

    Push

--- deploy-call-center/files/default/deployer.sh
+++ deploy-call-center/files/default/deployer.sh
@@ -1,12 +1,13 @@
-cd /srv/www/statc_app/current/ && bower install --allow-root --config.interactive=false
-cd /srv/www/statc_app/current/ && npm install
-cd /srv/www/statc_app/current/ && grunt 
+cd /srv/www/call_center_app/current/ && chown -R root:root *
+cd /srv/www/call_center_app/current/ && bower install --allow-root --config.interactive=false
+cd /srv/www/call_center_app/current/ && npm install
+cd /srv/www/call_center_app/current/ && grunt 
 rm -rfv /opt/tmp_store/
 mkdir /opt/tmp_store/
-cp -r /srv/www/statc_app/current/* /opt/tmp_store/
-rm -rfv /srv/www/statc_app/current/*
-rm -rfv /srv/www/statc_app/current/.*
-cp -r /opt/tmp_store/dist/* /srv/www/statc_app/current/
-cp -r /opt/tmp_store/app/fonts/* /srv/www/statc_app/current/fonts/
-cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/statc_app/current/fonts/
-chown deploy:www-data /srv/www/statc_app/current/*
+cp -r /srv/www/call_center_app/current/* /opt/tmp_store/
+rm -rfv /srv/www/call_center_app/current/*
+rm -rfv /srv/www/call_center_app/current/.*
+cp -r /opt/tmp_store/dist/* /srv/www/call_center_app/current/
+cp -r /opt/tmp_store/app/fonts/* /srv/www/call_center_app/current/fonts/
+cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/call_center_app/current/fonts/
+chown deploy:www-data /srv/www/call_center_app/current/*

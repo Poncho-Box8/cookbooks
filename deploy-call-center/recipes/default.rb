fd4e8c6f1cbbd19b29feb4c0d980aab4b4c71a79
aamirahmad170 <aamir@Aamirs-MacBook-Pro.local>
2015-10-17 12:46:48 +0530

    Push

--- deploy-call-center/recipes/default.rb
+++ deploy-call-center/recipes/default.rb
@@ -1,8 +1,8 @@
-cookbook_file "/tmp/lib-installer.sh" do
-  source "lib-installer.sh"
+cookbook_file "/tmp/deployer.sh" do
+  source "deployer.sh"
   mode 0755
 end
 
-execute "install my lib" do
-  command "sh /tmp/lib-installer.sh"
+execute "Deploy my app" do
+  command "sh /tmp/deployer.sh"
 end

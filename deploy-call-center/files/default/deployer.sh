fee68d0cc8cc46aa4eb9081007ebc4c67b40aaea
Aamir Kraipak <Aamir@Aamirs-MacBook-Pro.local>
2015-10-19 17:24:36 +0530

    test

--- deploy-call-center/files/default/deployer.sh
+++ deploy-call-center/files/default/deployer.sh
@@ -1,13 +1,13 @@
-cd /srv/www/sample_static_app/current && chown -R root:root *
-cd /srv/www/sample_static_app/current && bower install --allow-root --config.interactive=false
-cd /srv/www/sample_static_app/current && npm install
-cd /srv/www/sample_static_app/current && grunt 
+cd /srv/www/callcenter/current && chown -R root:root *
+cd /srv/www/callcenter/current && bower install --allow-root --config.interactive=false
+cd /srv/www/callcenter/current && npm install
+cd /srv/www/callcenter/current && grunt 
 rm -rfv /opt/tmp_store/
 mkdir /opt/tmp_store/
-cp -r /srv/www/sample_static_app/current/* /opt/tmp_store/
-rm -rfv /srv/www/sample_static_app/current/* 
-rm -rfv /srv/www/sample_static_app/current/.*
-cp -r /opt/tmp_store/dist/* /srv/www/sample_static_app/current/
-cp -r /opt/tmp_store/app/fonts/* /srv/www/sample_static_app/current/fonts/
-cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/sample_static_app/current/fonts/
-chown deploy:www-data /srv/www/sample_static_app/current/*
+cp -r /srv/www/callcenter/current/* /opt/tmp_store/
+rm -rfv /srv/www/callcenter/current/* 
+rm -rfv /srv/www/callcenter/current/.*
+cp -r /opt/tmp_store/dist/* /srv/www/callcenter/current/
+cp -r /opt/tmp_store/app/fonts/* /srv/www/callcenter/current/fonts/
+cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/callcenter/current/fonts/
+chown deploy:www-data /srv/www/callcenter/current/*

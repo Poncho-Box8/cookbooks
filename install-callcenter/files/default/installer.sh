735af04aaa9f5fa15594faadb6762cf4cbbf6d46
Aamir Kraipak <Aamir@Aamirs-MacBook-Pro.local>
2015-10-19 15:29:27 +0530

    Push

--- install-callcenter/files/default/installer.sh
+++ install-callcenter/files/default/installer.sh
@@ -5,8 +5,6 @@ n 0.12.4
 npm install -g npm@2.10.1
 git clone -b dev https://github.com/Poncho-Box8/Box8-CallCenter.git
 npm install -g bower grunt grunt-cli yo 
-bower install --allow-root
-npm install
 
 
 

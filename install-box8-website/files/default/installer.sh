a41af0d28a0661b22312877e27a2fe3091a14488
Aamir Kraipak <Aamir@Aamirs-MacBook-Pro.local>
2015-10-20 18:27:10 +0530

    New push

--- install-box8-website/files/default/installer.sh
+++ install-box8-website/files/default/installer.sh
@@ -1,10 +1,10 @@
 apt-get update -y
 apt-get install npm git  -y
 npm install -g n
-n 0.12.4
-npm install -g npm@2.10.1
-git clone -b dev https://github.com/Poncho-Box8/Box8-CallCenter.git
-npm install -g bower grunt grunt-cli yo 
+n 0.12.7
+npm install -g npm@2.12.1
+git clone -b updated https://github.com/Poncho-Box8/Box8-Web.git
+npm install -g grunt-cli bower yo generator-karma generator-angular
 
 
 

122f004dc31f64e4d828abc5634ca84ecf763750
Aamir Kraipak <Aamir@Aamirs-MacBook-Pro.local>
2015-10-20 19:32:01 +0530

    Push

--- deploy-call-center/files/default/deployer.sh
+++ deploy-call-center/files/default/deployer.sh
@@ -1,13 +1,11 @@
-cd /srv/www/callcenter/current && chown -R root:root *
-cd /srv/www/callcenter/current && bower install --allow-root --config.interactive=false
-cd /srv/www/callcenter/current && npm install
-cd /srv/www/callcenter/current && grunt 
+cd /srv/www/website_box8/current && chown -R root:root *
+cd /srv/www/website_box8/current && bower install --allow-root --config.interactive=false
+cd /srv/www/website_box8/current && npm install
+cd /srv/www/website_box8/current && grunt 
 rm -rfv /opt/tmp_store/
 mkdir /opt/tmp_store/
-cp -r /srv/www/callcenter/current/* /opt/tmp_store/
-rm -rfv /srv/www/callcenter/current/* 
-rm -rfv /srv/www/callcenter/current/.*
-cp -r /opt/tmp_store/dist/* /srv/www/callcenter/current/
-cp -r /opt/tmp_store/app/fonts/* /srv/www/callcenter/current/fonts/
-cp -r /opt/tmp_store/bower_components/font-awesome/fonts/* /srv/www/callcenter/current/fonts/
-chown deploy:www-data /srv/www/callcenter/current/*
+cp -r /srv/www/website_box8/current/* /opt/tmp_store/
+rm -rfv /srv/www/website_box8/current/* 
+rm -rfv /srv/www/website_box8/current/.*
+cp -r /opt/tmp_store/dist/* /srv/www/website_box8/current/
+chown deploy:www-data /srv/www/website_box8/current/*

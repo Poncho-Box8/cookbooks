bd517dddfe20b47adda6bcaccaee0709e850592a
Aamir Kraipak <Aamir@Aamirs-MacBook-Pro.local>
2015-10-21 10:20:33 +0530

    test

--- deploy-box8-website/files/default/deployer.sh
+++ deploy-box8-website/files/default/deployer.sh
@@ -7,7 +7,7 @@ mkdir /opt/tmp_store/
 cp -r /srv/www/website_box8/current/ /opt/tmp_store/
 rm -rfv /srv/www/website_box8/current/* 
 rm -rfv /srv/www/website_box8/current/.*
-cp -r /opt/tmp_store/dist/* /srv/www/website_box8/current/
+cp -r /opt/tmp_store/current/dist/* /srv/www/website_box8/current/
 chown deploy:www-data /srv/www/website_box8/current/*
 
 

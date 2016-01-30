1089fe944f11a3bd8d8daf6659458a5d5a1ee29b
root <root@public-instance.local>
2016-01-30 13:49:21 +0530

    Adding kafka_controller script

--- box8_scripts/recipes/default.rb
+++ box8_scripts/recipes/default.rb
@@ -2,9 +2,10 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
 bash "generate_docs" do
   user "root"
-  cwd "#{deploy[:deploy_to]}/current/config"
+  cwd "#{deploy[:deploy_to]}/current/"
   code <<-EOH
     bundle exec rake swagger:docs RAILS_ENV=production
+    bundle exec ruby kafka_order_controller.rb start
   EOH
 end
 end

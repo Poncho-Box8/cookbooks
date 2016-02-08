d7e981589719553e3b6b23d47223b7f89e03cc64
aahmad8 <aahmad@box8.in>
2016-02-08 18:04:30 +0530

    Updates
    Updates

--- box8_scripts/recipes/default.rb
+++ box8_scripts/recipes/default.rb
@@ -1,5 +1,9 @@
 include_recipe 'deploy'
 kafka_api = node[:deploy]['box8_api'][:environment_variables][:KAFKA_API]
+consumer_group = node[:deploy]['box8_api'][:environment_variables][:CONSUMER_GROUP]
+status_topic = node[:deploy]['box8_api'][:environment_variables][:STATUS_TOPIC]
+
+
 node[:deploy].each do |application, deploy|
 bash "generate_docs" do
   user "root"
@@ -7,7 +11,7 @@ bash "generate_docs" do
   code <<-EOH
     bundle exec rake swagger:docs RAILS_ENV=production
     /usr/bin/pkill -f kafka_reader.rb
-    KAFKA_API=#{kafka_api} bundle exec ruby kafka_reader_controller.rb start
+    KAFKA_API=#{kafka_api} CONSUMER_GROUP=#{consumer_group} STATUS_TOPIC=#{status_topic} bundle exec ruby kafka_reader_controller.rb start
   EOH
 end
 end
